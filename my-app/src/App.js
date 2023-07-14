f6762e2404e9c1788fbbccfd1305b4792a25882e
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-07-14 13:17:24 -0500

    no

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from'react';
 import './App.css';
 
 function App() {
+  const [img, setImg] = useState("")
+  useEffect(()=> {
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/2555`)
+    .then(response => response.json())
+    .then(data =>{
+      console.log(data)
+      setImg(data.primaryImage)})
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={img} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
