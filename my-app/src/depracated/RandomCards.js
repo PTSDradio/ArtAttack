9500c98633eefda7a8bbb83c31886608ad5a0da4
Will Chang <william.chaang@gmail.com>
2023-07-20 09:53:59 -0600

    Revert "Refactored RandomCards into GenerateNewCards"
    This reverts commit 161cc34bef24a5e3652e58d36a824bd000588353.

--- my-app/src/depracated/RandomCards.js
+++ my-app/src/depracated/RandomCards.js
@@ -1,57 +1,36 @@
-// import React, { useState, useEffect } from "react";
-// import CardDisplay from "./CardDisplay";
+import React, { useState, useEffect } from "react";
+import CardDisplay from "./CardDisplay";
 
-// function RandomCards() {
-//   const [cardPack, setCardPack] = useState([]);
+function RandomCards({ data }) {
+  const [cardPack, setCardPack] = useState([]);
 
-//   useEffect(() => {
-//     generateRandomCards();
-//   }, []);
+  useEffect(() => {
+    generateRandomCards();
+  }, []);
 
-//   const generateRandomCards = () => {
-//     let generatedCards = [];
-//     for (let i = 0; i < 3; i++) {
-//       const randomIndex = Math.floor(Math.random() * data.length);
-//       const randomObject = data[randomIndex];
-//       if (randomObject && randomObject.id) {
-//         generatedCards = [randomObject, ...generatedCards];
-//       }
-//     }
-//     setCardPack(generatedCards);
-//   };
+  const generateRandomCards = () => {
+    let generatedCards = [];
+    for (let i = 0; i < 3; i++) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomObject = data[randomIndex];
+      if (randomObject && randomObject.id) {
+        generatedCards = [randomObject, ...generatedCards];
+      }
+    }
+    setCardPack(generatedCards);
+  };
 
-//   const [data, setData] = useState([]);
+  return (
+    <div>
+      
+      <button onClick={generateRandomCards}>Random Cards</button>
+      <div>
+        {cardPack.map((data) => (
+          <CardDisplay data={data} key={data.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
-
- 
-// useEffect(() => {
-//   fetchCardPack();
-  
-// }, []);
-
-// const fetchCardPack = () => {
-
-//   fetch(`http://localhost:3000/cards`)
-//   .then(res => res.json())
-//   .then(data => {
-
-    
-//     setData(data);
-
-
-
-//   })
-// } 
-
-//   return (
-//     <div>
-//       <div>
-//         {cardPack.map((data) => (
-//           <CardDisplay data={data} key={data.id} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default RandomCards;
+export default RandomCards;
