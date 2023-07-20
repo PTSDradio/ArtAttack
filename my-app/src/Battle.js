ee102278d6e4ec4da61bcd4866bad6d28f12eb08
Will Chang <william.chaang@gmail.com>
2023-07-20 09:49:32 -0600

    Revert "added end of battle logic"
    This reverts commit cf818b7bc6fa22b46e7c03d564c5a092eff15637.

--- my-app/src/Battle.js
+++ my-app/src/Battle.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
-import CardDisplay from "./CardDisplay";
+import CPUcard from "./CPUcard";
 
 function Battle() {
   const [opponentCards, setOpponentCards] = useState([]);
-  const [playerCards, setPlayerCards] = useState([]);
-  const [battleState, setBattle] = useState([]);
+  const [userCards, setUserCard] = useState([]);
+  const [opponentState, setOpponent] = useState([]);
+  
 
   useEffect(() => {
     fetch("http://localhost:3000/players_cards")
       .then((res) => res.json())
       .then((data) => {
-        setPlayerCards(data);
+        setUserCard(data);
       });
 
     fetch("http://localhost:3000/opponents_cards")
@@ -22,86 +23,46 @@ function Battle() {
 
   const beginBattle = () => {
     console.log("begin battle");
-
-    const opponentMap = opponentCards.map((card) => {
-      return <CardDisplay key={card.id} array={card} />;
-    });
-    const playerMap = playerCards.map((card) => {
-      return <CardDisplay key={card.id} array={card} />;
-    });
-    const playerDivs = (
+    const opponentDivs = (
       <div>
-        <div>
-          <button className="generic-button" onClick={runBattle}>
-            {" "}
-            Fight!{" "}
-          </button>
-          <div>{opponentMap}</div>
-        </div>
-        <div>{playerMap}</div>
+        <button className="generic-button" onClick={runBattle}>
+          {" "}
+          Fight!{" "}
+        </button>
       </div>
     );
-    setBattle(playerDivs);
+    setOpponent(opponentDivs);
   };
 
   const runBattle = () => {
-
-    if (playerCards.length === 0) {
-        alert("You have no cards! Go buy some more cards!");
-    } else if (opponentCards.length === 0) {
-        alert("The opponenent has been destroyed! Challenge a new one!");
-    }
-    else {
     const currentPlayerCard =
-      playerCards[Math.floor(Math.random() * playerCards.length)];
+      userCards[Math.floor(Math.random() * userCards.length)];
 
     const currentOpponentCard =
       opponentCards[Math.floor(Math.random() * opponentCards.length)];
 
     if (currentPlayerCard.tier > currentOpponentCard.tier) {
-      alert("You have won!");
-      setOpponentCards(
-        opponentCards.splice(opponentCards.indexOf(currentOpponentCard), 1)
-        
-      );
-      beginBattle()
-      console.log(opponentCards);
+      window.alert("You have won!");
     } else if (currentPlayerCard.tier < currentOpponentCard.tier) {
-      alert("Your card has been defeated!");
-      setPlayerCards(
-        playerCards.splice(playerCards.indexOf(currentPlayerCard), 1)
-      );
-      beginBattle()
-      console.log(playerCards);
+      window.alert("Your card has been defeated!");
     } else {
       const playerNum = Math.floor(Math.random() * 5000);
       const opponentNum = Math.floor(Math.random() * 5000);
       if (playerNum > opponentNum) {
-        alert("You have won!");
-        setOpponentCards(
-          opponentCards.splice(opponentCards.indexOf(currentOpponentCard), 1)
-        );
-        beginBattle()
-        console.log(opponentCards);
+        window.alert("You have won!");
       } else if (playerNum < opponentNum) {
-        alert("Your card has been defeated!");
-        setPlayerCards(
-          playerCards.splice(playerCards.indexOf(currentPlayerCard), 1)
-        );
-        beginBattle()
-        console.log(playerCards);
+        window.alert("Your card has been defeated!");
       } else {
-        alert("You have tied!");
+        window.alert("You have tied!");
       }
     }
-  }
-}
+  };
   return (
     <div>
       <button className="generic-button" onClick={beginBattle}>
         Begin Battle
       </button>
-      {battleState}
+      {opponentState}
     </div>
   );
 
